4b654537f586f304b0351a5e9f5381cd4a830134
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 09:42:01 +0530

    Added: even or odd program

---
+++ src/evenorodd/EvenOdd.java
@@ -0,0 +1,23 @@
+package evenorodd;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);  
+		System.out.println("Enter any number");
+		int a = sc.nextInt();
+		
+		// If remainder is zero then this number is even
+		if (a % 2 == 0) {
+			System.out.println("Number is even");
+		}
+		
+		else {
+		// If remainder is not zero then this number is
+			System.out.println("number is odd");
+		}
+
+	}
+
+}
